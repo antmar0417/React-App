d6a7bac0038adc028b34d636005c1b029fdaf424
antmar0417 <tonyboy@live.se>
2022-04-30 17:52:02 +0200

    updated home page

---
+++ src/components/Clock/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Clock extends React.Component {
+  state = { time: new Date().toLocaleTimeString() };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ time: new Date().toLocaleTimeString() });
+    }, 1000);
+  }
+  render() {
+    if (this.state.time)
+      return <div className="time">Current time: {this.state.time}</div>;
+    else return <div>Loading...</div>;
+  }
+}
+export default Clock;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import View from "../../components/View";
+import Clock from "../../components/Clock";
 import "./home.css";
 
 const HomePage = (props) => {
   return (
     <View className="home-view">
+      <div>
+        <Clock />
+      </div>
       <div
         className="search-items"
-        style={{ width: "100%", marginBottom: "100px" }}
+        style={{ width: "100%", marginBottom: "100px", textAlign: "left" }}
       >
         <h1>This is the home page</h1>
       </div>
